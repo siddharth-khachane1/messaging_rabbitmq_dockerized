dccef2f7f389260d44067dd61e956ff8c08be75a
siddharth-khachane1 <104891095+siddharth-khachane1@users.noreply.github.com>
2024-07-13 21:48:03 +0530

    init

---
+++ src/main/java/com/app/messaging_rabbitmq_dockerized/MessagingRabbitmqDockerizedApplication.java
@@ -0,0 +1,13 @@
+package com.app.messaging_rabbitmq_dockerized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingRabbitmqDockerizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingRabbitmqDockerizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/messaging_rabbitmq_dockerized/MessagingRabbitmqDockerizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.messaging_rabbitmq_dockerized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRabbitmqDockerizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
